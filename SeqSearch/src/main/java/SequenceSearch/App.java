f5b2173fb03bffe078dfaf7186dec4728c0df529
tommytcao <44215485+tommytcao@users.noreply.github.com>
2019-10-15 15:17:32 -0700

    Add files via upload

---
+++ SeqSearch/src/main/java/SequenceSearch/App.java
@@ -0,0 +1,32 @@
+package SequenceSearch;
+
+import java.io.File;
+import java.nio.file.Files;
+
+/**
+ * Tommy Cao
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+//        Files folder = new File();
+//        File[] listOfFiles = folder.listFiles();
+//
+//        for (int i = 0; i < listOfFiles.length; i++) {
+//            File file = listOfFiles[i];
+//            if (file.isFile() && file.getName().endsWith(".txt")) {
+//                String content = FileUtils.readFileToString(file);
+//            }
+//        }
+        int num = 100;
+        char sequence[] = "IT*TDIDDDMN".toCharArray();
+        for (int i = 0; i < num; i++){
+
+        }
+
+
+
+    }
+}
---
+++ SeqSearch/src/test/java/SequenceSearch/AppTest.java
@@ -0,0 +1,20 @@
+package SequenceSearch;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
